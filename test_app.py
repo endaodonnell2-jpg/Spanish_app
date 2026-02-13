c3bd97e3db7696074487cea494c8a66147812328
endaodonnell2-jpg <endaodonnell2@gmail.com>
2026-02-13 15:46:49 +0100

    Delete test_app.py

--- test_app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from hold_to_speak_component.hold_to_speak import HoldToSpeak
-
-st.title("Test Hold-to-Speak Component")
-
-hts = HoldToSpeak()
-audio_b64 = hts.call()
-
-st.write("audio_b64 type:", type(audio_b64))
