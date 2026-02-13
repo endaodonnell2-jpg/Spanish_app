1d091715bce5ac152c339baf578bb605e382ad06
endaodonnell2-jpg <endaodonnell2@gmail.com>
2026-02-13 11:48:51 +0100

    Perfect hold to speakapp.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
-#
+
 st.set_page_config(page_title="Spanish App", layout="centered")
 
 # The entire logic is now inside this block to avoid Python Indent Errors
